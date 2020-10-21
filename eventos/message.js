7a507955aba158894d5eb8eb453312dad42100c7
Monoverde <minecraftersov@gmail.com>
2020-10-21 20:09:04 -0300

    añadir enviar dms a un canal

--- eventos/message.js
+++ eventos/message.js
@@ -1,20 +1,28 @@
-//Esta función se activara por cada mensaje enviado en un canal por el usuario:
-module.exports = (client, message) => { 
-
-    if(!message.content.startsWith(client.config.prefix)) return; 
-    if(message.author.bot) return;
-  
-    // Definiendo los argumentos y comandos.
-    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);  
-    const command = args.shift().toLowerCase()
-  
-    // Manejando los eventos.
-    let cmd = client.comandos.get(command); // Obtiene el comando de la colección client.commandos
-    if(!cmd) return; // Si no hay comandos no ejecute nada.
-    
-    // Ejecuta el comando enviando el client, el mensaje y los argumentos.
-    cmd(client, message, args);
-    
-  
+const Discord = require("discord.js")
+module.exports = (client, message) => {
+  if (message.channel.type == "dm") {
+    let embed = new Discord.MessageEmbed()
+      .setTitle("Nuevo DM!")
+      .setThumbnail(message.author.avatarURL())
+      .setColor("RANDOM")
+      .addField("Contenido:", message.content, true)
+      .addField("Autor:", message.author.tag, true)
+      .addField("ID:", message.author.id, true)
+    return client.channels.cache.get("738032942776975370").send(embed)
   }
-  
+  if (!message.content.startsWith(client.config.prefix)) return;
+  if (message.author.bot) return;
+
+  // Definiendo los argumentos y comandos.
+  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase()
+
+  // Manejando los eventos.
+  let cmd = client.comandos.get(command); // Obtiene el comando de la colección client.commandos
+  if (!cmd) return; // Si no hay comandos no ejecute nada.
+
+  // Ejecuta el comando enviando el client, el mensaje y los argumentos.
+  cmd(client, message, args);
+
+
+}
--- thalas-support.js
+++ thalas-support.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-// El modulo fs se utiliza para leer los archivos y carpetas de un directorio:
-let { readdirSync } = require('fs'); 
+let {
+  readdirSync
+} = require('fs');
 
 
 process.on('SIGTERM', () => {
@@ -10,73 +11,52 @@ process.on('SIGTERM', () => {
   })
 })
 
-// Referenciamos nuestro archivo de configuración, ahora en JS:
-client.config = require('./config.js'); 
 
-//Creamos una colección para Discordjs llamada 'comandos':
-client.comandos = new Discord.Collection();  
+client.config = require('./config.js');
 
-// <-- AQUI EL CONTROLADOR DE COMANDOS: -->
+client.comandos = new Discord.Collection();
 
-// <-- CONTROLADOR DE COMANDOS: -->
 
-//dentro de nuestro for llamamos a la carpeta comandos creada:
-for(const file of readdirSync('./comandos/')) { 
 
-  //Esta condición evitara que los archivos que no son tengan la extención .js no sean listado:
-  if(file.endsWith(".js")) { 
+for (const file of readdirSync('./comandos/')) {
 
-  //Elimina los últimos tres caracteres nombre del archivo para
-  //deshacerse de la extensión .js y solo quedarnos con el nombre del comando:
-  let fileName = file.substring(0, file.length - 3); 
 
-  //Define una nueva varible 'fileContents' de la exportación del comando 
-  //dentro de la carpeta comandos:
-  let fileContents = require(`./comandos/${file}`); 
+  if (file.endsWith(".js")) {
 
-  //Agrega el nombre del comando a la colección client.commands con un 
-  //valor de sus exportaciones respectivas.
-  client.comandos.set(fileName, fileContents);
+
+    let fileName = file.substring(0, file.length - 3);
+
+
+    let fileContents = require(`./comandos/${file}`);
+
+    client.comandos.set(fileName, fileContents);
   }
 }
 
-// <-- AQUI EL CONTROLADOR DE EVENTOS: -->
 
-// <-- CONTROLADOR DE EVENTOS: -->
+for (const file of readdirSync('./eventos/')) {
 
-//dentro de nuestro for llamamos a la carpeta eventos creada:
-for(const file of readdirSync('./eventos/')) { 
 
-  //Esto condicion evitara que los archivos que no son archivos .js no coleccione:
-  if(file.endsWith(".js")){
+  if (file.endsWith(".js")) {
 
-  //Elimina los últimos tres caracteres nombre del archivo para
-  //deshacerse de la extensión .js y solo quedarnos con el nombre del evento:
-  let fileName = file.substring(0, file.length - 3); 
 
-  //Define una nueva variable 'fileContents' de la exportación del evento dentro de la carpeta eventos:
-  let fileContents = require(`./eventos/${file}`); 
-  
-  // Cuando el evento se activa o es solicitada exportamos la función con 
-  // el nombre del evento vinculada y tambien el parametro client.
-  client.on(fileName, fileContents.bind(null, client)); 
-		
-  // Elimina la memoria caché del archivo requerido para facilitar la recarga y no 
-  // tener más memoria de la necesaria.
-  delete require.cache[require.resolve(`./eventos/${file}`)]; 
+    let fileName = file.substring(0, file.length - 3);
+
+    let fileContents = require(`./eventos/${file}`);
+
+    client.on(fileName, fileContents.bind(null, client));
+
+
+    delete require.cache[require.resolve(`./eventos/${file}`)];
   }
 }
 
-// <-- AQUI LA PROPIEDAD LOGIN: -->
 
-// <-- PROPIEDAD LOGIN: -->
-
-// Inicia sesión en Discord con el token definido en config.
-client.login() //agregamos las promesas de la propiedad login.
-  .then(() => { 
+client.login()
+  .then(() => {
     console.log(`Thalas es calvo o yo no me llamo ${client.user.tag}`);
     client.user.setPresence({
-      status:"idle",
+      status: "idle",
       activity: {
         name: `en desarrollo 👀 || twitch.tv/soythalas`,
         type: "WATCHING"
@@ -85,7 +65,7 @@ client.login() //agregamos las promesas de la propiedad login.
   })
   .catch((err) => {
 
-    //Si se produce un error al iniciar sesión, se le indicará en la consola.
+
     console.error("Error al iniciar sesión: " + err);
 
   });
