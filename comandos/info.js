30467b92e751e237246af42b1080c0eb56562fd0
Monoverde <minecraftersov@gmail.com>
2020-10-22 13:11:16 -0300

    uy

--- comandos/info.js
+++ comandos/info.js
@@ -4,6 +4,9 @@ module.exports = async (client, message) => {
         .setTitle("SoyThalas | Información")
         .setDescription("Hola!! Yo soy `ThalasSupport#0362`, fui creado por `DonWeon#1234` y `Monoverde#8888`para ayudar a los miembros del servidor de **SoyThalas**. Puedes ver todos mis comandos usando `st!help` ")
         .setThumbnail("https://cdn.discordapp.com/avatars/765944857746997268/faa316ea6865a2d57494776ebf4f0c07.png?size=256")
+        .addField('Desarrolladores', 'DonWeon#1234 y Monoverde#8888')
+        .addField('Foto de Perfil', '🍭 ᴿᴴ ᴢᴜᴅᴏᴋᴀᴛᴏ 🍬#2938')
+        .addField('Hosting', 'Docker {Monoverde#8888}')
         .setColor("FFCC33")
 
     await message.channel.send(embed)
